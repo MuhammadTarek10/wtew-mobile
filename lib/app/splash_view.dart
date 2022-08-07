2c342252748d120c3212009ecd652566a9a09866
Muhammad Tarek <muhammadtarek@Muhammads-MacBook-Air.local>
2022-08-07 19:13:27 +0200

    made values instead of valures

--- lib/app/splash_view.dart
+++ lib/app/splash_view.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:wtew22/config/app_routes.dart';
 import 'package:wtew22/config/utils/app_assets.dart';
 import 'package:wtew22/config/utils/app_colors.dart';
-import 'package:wtew22/config/utils/app_valures.dart';
+import 'package:wtew22/config/utils/app_values.dart';
 
 class SplashView extends StatefulWidget {
   const SplashView({Key? key}) : super(key: key);
