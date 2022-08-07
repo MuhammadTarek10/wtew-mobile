2af9625165c416586d938fe4d866edaee1284981
Muhammad Tarek <muhammadtarek@Muhammads-MacBook-Air.local>
2022-08-07 19:12:44 +0200

    added day card

--- lib/features/wetw22/presentation/views/home_view.dart
+++ lib/features/wetw22/presentation/views/home_view.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:wtew22/config/app_routes.dart';
-import 'package:wtew22/config/utils/app_colors.dart';
 import 'package:wtew22/config/utils/app_strings.dart';
 import 'package:wtew22/features/wetw22/domain/entities/activity.dart';
 import 'package:wtew22/features/wetw22/domain/entities/day.dart';
 import 'package:wtew22/features/wetw22/domain/entities/note.dart';
+import 'package:wtew22/features/wetw22/presentation/widgets/day_card.dart';
 
 class HomeView extends StatelessWidget {
   HomeView({Key? key}) : super(key: key);
@@ -223,52 +222,11 @@ class HomeView extends StatelessWidget {
         title: const Text(AppStrings.homeViewTitle),
       ),
       body: ListView.builder(
-        itemBuilder: (context, index) => HomeCard(
+        itemBuilder: (context, index) => DayCard(
           day: days[index],
           length: days.length,
         ),
         itemCount: days.length,
-      ),
-    );
-  }
-}
-
-class HomeCard extends StatelessWidget {
-  const HomeCard({
-    Key? key,
-    required this.day,
-    required this.length,
-  }) : super(key: key);
-
-  final Day day;
-  final int length;
-
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () =>
-          Navigator.pushNamed(context, AppRoutes.timelineRoute, arguments: day),
-      child: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(8),
-          child: Container(
-            width: double.infinity,
-            height: MediaQuery.of(context).size.height / (length + 1),
-            decoration: BoxDecoration(
-              color: AppColors.primaryColor,
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  day.name,
-                  style: Theme.of(context).textTheme.headline6,
-                ),
-              ],
-            ),
-          ),
-        ),
       ),
     );
   }
---
+++ lib/features/wetw22/presentation/widgets/day_card.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:wtew22/config/app_routes.dart';
+import 'package:wtew22/config/utils/app_colors.dart';
+import 'package:wtew22/features/wetw22/domain/entities/day.dart';
+
+class DayCard extends StatelessWidget {
+  const DayCard({
+    Key? key,
+    required this.day,
+    required this.length,
+  }) : super(key: key);
+
+  final Day day;
+  final int length;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () =>
+          Navigator.pushNamed(context, AppRoutes.timelineRoute, arguments: day),
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: Container(
+            width: double.infinity,
+            height: MediaQuery.of(context).size.height / (length + 1),
+            decoration: BoxDecoration(
+              color: AppColors.primaryColor,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  day.name,
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
