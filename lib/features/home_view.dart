2f50e52c0d4ed9380c7733fd5b15ca5b165e027d
Muhammad Tarek <mohammadtarek000@gmail.com>
2022-09-13 12:47:40 +0200

    reorganized files

---
+++ lib/features/home_view.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:wtew22/config/app_routes.dart';
+import 'package:wtew22/config/utils/app_assets.dart';
+import 'package:wtew22/config/utils/app_constants.dart';
+import 'package:wtew22/config/utils/app_strings.dart';
+import 'package:wtew22/features/drawer_header.dart';
+import 'package:wtew22/features/drawer_item.dart';
+import 'package:wtew22/features/talks/domain/entities/sections.dart';
+import 'package:wtew22/features/talks/presentation/widgets/section_card.dart';
+import 'package:wtew22/features/talks/presentation/widgets/sections/section_list_view.dart';
+
+// ignore: must_be_immutable
+class HomeView extends StatefulWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  List<BaseSection> sections = [
+    AppConstants.subjects,
+    AppConstants.comparisons,
+    AppConstants.softSkills,
+    AppConstants.gpa,
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(AppStrings.welcomeMessage),
+        actions: [AppConstants.buildHelpButton(context)],
+      ),
+      body: SecionListView(sections: sections),
+      drawer: buildDrawer(context),
+    );
+  }
+
+  Drawer buildDrawer(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          const Header(),
+          const Divider(),
+          DrawerItem(
+            title: AppStrings.gpaCalculator,
+            icon: AppAssets.gpaIcon,
+            onTap: () =>
+                Navigator.pushNamed(context, AppRoutes.gpaCalculatorRoute),
+          ),
+          DrawerItem(
+            title: AppStrings.map,
+            icon: AppAssets.map,
+            onTap: () {},
+          ),
+          DrawerItem(
+            title: AppStrings.partners,
+            icon: AppAssets.partners,
+            onTap: () {},
+          ),
+          DrawerItem(
+            title: AppStrings.settings,
+            icon: AppAssets.settings,
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class HomeSecions extends StatelessWidget {
+  const HomeSecions({
+    Key? key,
+    required this.sections,
+  }) : super(key: key);
+
+  final List<BaseSection> sections;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            Section(section: sections[0]),
+            Section(section: sections[1]),
+          ],
+        ),
+        Row(
+          children: [
+            Section(section: sections[2]),
+            Section(section: sections[3]),
+          ],
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/features/talks/presentation/widgets/sections/section_list_view.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:wtew22/config/utils/app_media_query.dart';
+import 'package:wtew22/features/talks/domain/entities/sections.dart';
+import 'package:wtew22/features/talks/presentation/widgets/section_card.dart';
+import 'package:wtew22/features/talks/presentation/widgets/sections/section_animated_scroll_view.dart';
+
+class SecionListView extends StatelessWidget {
+  const SecionListView({
+    Key? key,
+    required this.sections,
+  }) : super(key: key);
+
+  final List<BaseSection> sections;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      itemCount: sections.length,
+      separatorBuilder: (context, index) =>
+          Divider(height: context.height * 0.005),
+      itemBuilder: (context, index) {
+        final section = sections[index];
+        return SectionAnimatedScrollView(
+          child: SectionCard(
+            section: section,
+          ),
+        );
+      },
+    );
+  }
+}
