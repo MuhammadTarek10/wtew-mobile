8487b7fee4e25b1156c40a6df21fb336531df7ea
Muhammad Tarek <mohammadtarek000@gmail.com>
2025-03-10 15:29:24 +0200

    The real upgrade

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
 
-        minSdk = flutter.minSdkVersion
+        minSdk = 34
         targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
